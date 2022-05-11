a798137158fea3a7c4f2339099043dc6d2597ab7
lucas.braatz <lucas.braatz@DESKTOP-S3H381L>
2022-05-11 15:22:38 -0300

    Adicionar arquivos de projeto.

---
+++ Exercício 01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercício_01 {
+    class Program {
+        static void Main(string[] args) {
+
+            int initialVolume, pumpFlow, time, poisonVolume;
+
+            initialVolume = int.Parse(Console.ReadLine());
+            pumpFlow = int.Parse(Console.ReadLine());
+            time = int.Parse(Console.ReadLine());
+
+            poisonVolume = initialVolume - (pumpFlow * time);
+
+            if (poisonVolume < 0) {
+                Console.WriteLine("nao eh possivel");
+            }
+            else {
+                Console.WriteLine(poisonVolume);
+            }
+        }
+    }
+}
