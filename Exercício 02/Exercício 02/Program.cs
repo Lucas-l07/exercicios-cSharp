5610fbdefb0bc57f1647a0e4e63130d84aa64eb9
lucas.braatz <lucas.braatz@DESKTOP-S3H381L>
2022-05-13 11:31:12 -0300

    teste

---
+++ Exercício 02/Exercício 02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercício_02 {
+    class Program {
+        static void Main(string[] args) {
+            double note1, note2, note3, finalNote;
+            //lucas
+            string[] vet = Console.ReadLine().Split(' ');
+            note1 = double.Parse(vet[0]);
+            note2 = double.Parse(vet[1]);
+            note3 = double.Parse(vet[2]);
+
+            finalNote = note1 + note2 + note3;
+
+            Console.WriteLine(finalNote.ToString("F2"));
+
+            if (finalNote < 60.00) {
+                Console.WriteLine("DEPENDENCIA");
+            }
+        }
+    }
+}
