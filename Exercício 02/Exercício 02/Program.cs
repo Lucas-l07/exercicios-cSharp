2a1d7d4a72b0603a24e2e6572151aaaf774bcef8
lucas.braatz <lucas.braatz@DESKTOP-S3H381L>
2022-05-20 14:01:55 -0300

    Exercídio 03

--- Exercício 02/Exercício 02/Program.cs
+++ Exercício 02/Exercício 02/Program.cs
@@ -4,19 +4,24 @@ namespace Exercício_02 {
     class Program {
         static void Main(string[] args) {
             double note1, note2, note3, finalNote;
-            //lucas braatz
-            string[] vet = Console.ReadLine().Split(' ');
+
+            string[] vet = Console.ReadLine().Replace('.', ',').Split(' ');
             note1 = double.Parse(vet[0]);
             note2 = double.Parse(vet[1]);
             note3 = double.Parse(vet[2]);
 
-            finalNote = note1 + note2 + note3;
+            finalNote = double.Parse(teste(note1, note2, note3).ToString("F2"));
 
-            Console.WriteLine(finalNote.ToString("F2"));
+            Console.WriteLine(finalNote);
 
             if (finalNote < 60.00) {
                 Console.WriteLine("DEPENDENCIA");
             }
         }
+        public static double teste(double note1, double note2, double note3) {
+            double finalNote = note1 + note2 + note3;
+            return finalNote;
+        }
+
     }
 }
--- Exercício 03/Exercício 03/Program.cs
+++ Exercício 03/Exercício 03/Program.cs
@@ -3,7 +3,36 @@
 namespace Exercício_03 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            int widthPhoto, heightPhoto, widthPictureFrame, heightPictureFrame;
+            string[] vet1 = Console.ReadLine().Split(' ');
+            string[] vet2 = Console.ReadLine().Split(' ');
+
+            Program teste = new Program();
+            widthPhoto = int.Parse(vet1[0]);
+            heightPhoto = int.Parse(vet1[1]);
+            widthPictureFrame = int.Parse(vet2[0]);
+            heightPictureFrame = int.Parse(vet2[1]);
+
+            if (teste.SizeVerify(widthPhoto, heightPhoto, widthPictureFrame, heightPictureFrame) == false) {
+                widthPhoto = int.Parse(vet1[1]);
+                heightPhoto = int.Parse(vet1[0]);
+
+                if (teste.SizeVerify(widthPhoto, heightPhoto, widthPictureFrame, heightPictureFrame) == true) {
+                    Console.WriteLine("SIM");
+                }
+                else {
+                    Console.WriteLine("NAO");
+                }
+            }
+            else {
+                Console.WriteLine("SIM");
+            }
         }
+        public bool SizeVerify(int widthPhoto, int heightPhoto, int widthPictureFrame, int heightPictureFrame) {
+            return (widthPhoto <= widthPictureFrame && heightPhoto <= heightPictureFrame);
+        }
+
     }
+
 }
+
