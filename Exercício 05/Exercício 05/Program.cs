e2fbe6d1b15f94cff753e42daf62deaaa97baf78
lucas.braatz <lucas.braatz@DESKTOP-S3H381L>
2022-05-23 14:34:42 -0300

    Exercício 05

---
+++ Exercício 05/Exercício 05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exercício_05 {
+    class Program {
+        static void Main(string[] args) {
+            double highestValue = 0;
+            string[] vetor = Console.ReadLine().Split(' ');
+
+            // vector reading
+            for(int i=0; i < vetor.Length; i++) {
+                if(double.Parse(vetor[i]) > highestValue) {
+                    highestValue = double.Parse(vetor[i]);
+                }
+            }
+
+            Console.WriteLine(highestValue.ToString("F2"));
+        }
+    }
+}
