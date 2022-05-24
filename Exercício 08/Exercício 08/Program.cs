3146dc09bd6225528152d73b00fae5d06dc19b91
lucas.braatz <lucas.braatz@DESKTOP-S3H381L>
2022-05-24 12:00:29 -0300

    Exercício 08

---
+++ Exercício 08/Exercício 08/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercício_08 {
+    class Program {
+        static void Main(string[] args) {
+            int n, a, b;
+
+            n = int.Parse(Console.ReadLine());
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            int i = a;
+            while (i <= b) {
+                if (i % n == 0) {
+                    Console.WriteLine(i);
+                }
+                i += n;
+            }
+        }
+    }
+}
