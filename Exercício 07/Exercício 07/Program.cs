1a992170f2488d71fd3e28f4932ac745cf9c9e37
lucas.braatz <lucas.braatz@DESKTOP-S3H381L>
2022-05-24 11:40:43 -0300

    Exercício 07

---
+++ Exercício 07/Exercício 07/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercício_07 {
+    class Program {
+        static void Main(string[] args) {
+            int num = 1;
+
+            while (num != 0) {
+                num = int.Parse(Console.ReadLine());
+                Console.WriteLine(Math.Pow(num, 2));
+            }
+        }
+    }
+}
