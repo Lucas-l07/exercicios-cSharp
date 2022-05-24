b817cdd84fd1895ab890adac0e5688a431023ee4
lucas.braatz <lucas.braatz@DESKTOP-S3H381L>
2022-05-24 11:24:30 -0300

    Exercício 06

---
+++ Exercício 06/Exercício 06/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercício_06 {
+    class Program {
+        static void Main(string[] args) {
+            int sum=0;
+            string[] vetor = Console.ReadLine().Split(' ');
+
+            for (int i = int.Parse(vetor[0]); i <= int.Parse(vetor[1]); i++) {
+                if (i % 2 != 0) {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
