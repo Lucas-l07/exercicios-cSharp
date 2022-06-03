972557ec34dd80420cc79c8be99ff8b8bf47aa4a
lucas.braatz <lucas.braatz@DESKTOP-S3H381L>
2022-06-03 13:14:28 -0300

    Exercício 09

---
+++ Exercício 09/Exercício 09/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercício_09 {
+    class Program {
+        static void Main(string[] args) {
+            double totalBatteryTime;
+            int timesToPlay;
+            double[] vetor;
+
+            totalBatteryTime = double.Parse(Console.ReadLine());
+            timesToPlay = int.Parse(Console.ReadLine());
+            vetor = new double[timesToPlay];
+
+            for (int i = 0; i < timesToPlay; i++) {
+                vetor.SetValue(double.Parse(Console.ReadLine()), i);
+                totalBatteryTime -= vetor[i];
+
+                if (totalBatteryTime < 0) {
+                    Console.WriteLine("recarregar");
+                }
+                else {
+                    Console.WriteLine(totalBatteryTime.ToString("F1"));
+                }
+            }
+
+        }
+    }
+}
