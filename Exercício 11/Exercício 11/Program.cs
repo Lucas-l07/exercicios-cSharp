ed4d3d3bbb7ef0e19f15d1fc90315d06f3086877
lucas.braatz <lucas.braatz@DESKTOP-S3H381L>
2022-06-06 12:07:41 -0300

    Exercício 11

---
+++ Exercício 11/Exercício 11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercício_11 {
+    class Program {
+        static void Main(string[] args) {
+            int volume;
+            int command;
+
+            volume = int.Parse(Console.ReadLine());
+            command = int.Parse(Console.ReadLine());
+
+            while (command != 0) {
+                
+                volume += command;
+
+                if (volume > 100) {
+                    volume = 100;
+                }
+                if (volume < 0) {
+                    volume = 0;
+                }
+
+                Console.WriteLine(volume);
+                command = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
